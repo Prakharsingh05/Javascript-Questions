3d78ca82f3708001f0af96362bf948b8e30150e3
Prakhar Singh <prakharsingh285@gmail.com>
2023-07-30 21:31:56 +0530

    Added a comment to explain the output of the console.log statement, and added an example of how to use the `push()` method to add an element to an array.

--- Arrays.js
+++ Arrays.js
@@ -8,3 +8,8 @@ var myArray = [1,2,3];
 //We can access individual elements of an array by their index (position) within the bracket notation
 
 console.log(myArray[0]);
+// Output: "1" - The first element has an index of zero
+
+
+//You can also add elements to an array using the `push()` method. The `push()` method takes a single argument, which is the value to be added to the array. For example, the following code adds the number 4 to the `myArray` array:
+myArray.push(4);
