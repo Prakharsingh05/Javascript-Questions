d0cf2fc8b7e729bef18f74ef3d33fb5bbc66fb0f
Prakhar Singh <prakharsingh285@gmail.com>
2023-07-30 21:23:25 +0530

    Added access with brackets notation to obj.

--- ObjType.js
+++ ObjType.js
@@ -11,3 +11,17 @@ obj.b; // 123
 obj.c; // true
 
 obj["a"]; // "hello world " , accesed with brackets notation
+obj["b"]; // 123
+obj["c"]; //true
+
+//Brackets notaion is also useful when you eant to acces property / key but the name is store in another variable , such as:
+
+var obj = {
+    a: "hello world",
+    b : 123,
+};
+
+var b = "a";
+
+obj[b]; // "helloworld"
+obj["b"]; // 123
