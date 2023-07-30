3fdf7c5870e53c0b94acc337f0a6d573c0773f36
Prakhar Singh <prakharsingh285@gmail.com>
2023-07-30 21:39:54 +0530

    Added an example of how to use the `typeof` operator.

--- Operators.js
+++ Operators.js
@@ -13,4 +13,14 @@
 // - `"function"`: the operand is a function
 // - `"object"`: the operand is an object or null
 
+// For example, the following code uses the `typeof` operator to check the type of a variable:
 
+var myVariable = "hello";
+console.log(typeof myVariable); // "string"
+
+
+//The `typeof` operator can also be used with expressions. For example, 
+//the following code uses the `typeof` operator to check if a variable is an object:
+
+var myObject = { name: "John Doe" };
+console.log(typeof myObject); // "object"
