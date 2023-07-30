236da7036aaa7c82819fb5b2daf7755465922570
Prakhar Singh <prakharsingh285@gmail.com>
2023-07-30 21:13:16 +0530

    Added a prototype object and a new object that inherits from it.

--- Prototype.js
+++ Prototype.js
@@ -1,3 +1,22 @@
 //What is prototype ?
 //
-var myObject = new Object();
+
+var myObject = {
+    // This is a property of the object.
+    property: "value",
+  
+    // This is a method of the object.
+    method: function() {
+      // This is the body of the method.
+    }
+  };
+  
+  // This is the prototype object.
+  var prototype = Object.create(myObject);
+  
+  // This is a new object that inherits from the prototype object.
+  var newObject = Object.create(prototype);
+  
+  // The new object has the same properties and methods as the prototype object.
+  console.log(newObject.property);
+  newObject.method();
