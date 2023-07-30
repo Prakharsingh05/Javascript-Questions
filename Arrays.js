bd40e9e41f988b2b6c51b85c736dd5360dab3475
Prakhar Singh <prakharsingh285@gmail.com>
2023-07-30 21:27:55 +0530

    Added an explanation of arrays in JavaScript.

---
+++ Arrays.js
@@ -0,0 +1,10 @@
+//Explain arrays in javascript?
+
+// An array is a data structure that stores data in a sequential order. 
+// In JavaScript, arrays are defined using the square brackets `[]`. 
+// For example, the following code defines an array called `myArray` that stores the numbers 1, 2, and 3:
+
+var myArray = [1,2,3];
+//We can access individual elements of an array by their index (position) within the bracket notation
+
+console.log(myArray[0]);
