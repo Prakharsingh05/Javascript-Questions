45e47b77a54051813ab8edbd140253c073b144c0
Prakhar Singh <prakharsingh285@gmail.com>
2023-07-30 21:19:28 +0530

    Added comments to explain the code.

--- ObjType.js
+++ ObjType.js
@@ -6,4 +6,8 @@ var obj={
     c:true
 };
 
-obj.a;  // 
+obj.a;  // " hello world" , accessed with doted notation
+obj.b; // 123
+obj.c; // true
+
+obj["a"]; // "hello world " , accesed with brackets notation
