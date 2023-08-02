ae4f16b0850512e630472cb7b36b6bd0062015df
Prakhar Singh <prakharsingh285@gmail.com>
2023-08-02 23:00:27 +0530

    load event vs dom event

--- LoadEvent_Vs_DomEvent.js
+++ LoadEvent_Vs_DomEvent.js
@@ -4,6 +4,7 @@
 
 
 //Answer:-
+
 // The load event is fired when the entire document has finished loading, including all images and other resources. The DOMContentLoaded event is fired when the document has finished loading, but before images and other resources have finished loading.
 
 // The advantage of the load event is that it is guaranteed to be fired after all resources have finished loading. This can be important for scripts that need to access images or other resources that are loaded after the document has finished loading.
