50e22abcc86c3cdc46376ef21962bf95e5641301
Prakhar Singh <prakharsingh285@gmail.com>
2023-08-02 22:52:25 +0530

    Keywords

---
+++ KeyWords.js
@@ -0,0 +1,24 @@
+//what are different keywords and thier uses define with example?
+
+Sure, here are some of the most common keywords in JavaScript and their uses:
+
+// * `var`: This keyword is used to declare variables.\
+
+// * `let`: This keyword is also used to declare variables, but it has some additional features 
+//that make it more useful in certain situations. For example, 
+//`let` variables are block-scoped, which means that they can only be accessed within the block in which they are declared.
+
+
+// * `const`: This keyword is used to declare constants. Constants are variables that cannot be changed after they are declared.
+
+// * `function`: This keyword is used to declare functions. Functions are blocks of code that can be executed multiple times.
+
+// * `return`: This keyword is used to return a value from a function.
+
+// * `if`: This keyword is used to create conditional statements. Conditional statements allow you to execute different code depending on whether or not a condition is true.
+
+// * `else`: This keyword is used to specify the code that should be executed if the condition in an `if` statement is false.
+
+// * `for`: This keyword is used to create loops. Loops allow you to execute code multiple times.
+
+// * `while`: This keyword is also used to create loops, but it works differently than `for` loops. `while` loops continue to execute code as long as a condition is true.
