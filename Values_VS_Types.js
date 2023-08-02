f1ea0f43d2eb2d4198e1f00c2cbc935503d3e994
Prakhar Singh <prakharsingh285@gmail.com>
2023-08-02 22:47:33 +0530

    Question 7

---
+++ Values_VS_Types.js
@@ -0,0 +1,10 @@
+//Explain Values and Types in Javascript ES6?
+
+//In JavaScript, a value is a piece of data that can be represented by a variable. A type is a classification of values based on their characteristics. In JavaScript, there are six data types:
+
+// * Number
+// * String
+// * Boolean
+// * Null
+// * Undefined
+// * Object
