3dc4b4a66541f353cccad890906ee20a95807e3d
Prakhar Singh <prakharsingh285@gmail.com>
2023-08-03 23:09:56 +0530

    event delegation

---
+++ EventDelehgation.js
@@ -0,0 +1,3 @@
+//event delegation?
+
+In JavaScript, event delegation is a technique for handling events in a web page. Event delegation is used when a single event listener is added to a parent element, instead of adding event listeners to each child element. This reduces the amount of memory used by the page and improves the performance of the page.
