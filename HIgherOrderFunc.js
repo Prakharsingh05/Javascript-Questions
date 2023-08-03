3205ed9e2c23e0e51ed9f3e31a9dc9e3fbdf0941
Prakhar Singh <prakharsingh285@gmail.com>
2023-08-03 23:09:48 +0530

    hof

--- HIgherOrderFunc.js
+++ HIgherOrderFunc.js
@@ -1,1 +1,13 @@
-//explin higher order function?
+// //explin higher order function?
+
+// In JavaScript, a higher-order function is a function that takes a function as an argument or returns a function. Higher-order functions are functions that either take a function as an argument or return a function. These functions are called "first-class functions" in JavaScript because they can be treated like any other value in the language.
+
+// In JavaScript, higher-order functions are used extensively in functional programming. They are used for creating abstractions, for example, to create a map function that applies a function to each element of an array, or to create a filter function that selects only the elements that satisfy a certain condition.
+
+// In JavaScript, the following are examples of higher-order functions:
+
+// - Array methods like map, filter, reduce, and forEach.
+// - Function.prototype.bind, which returns a new function that, when called, binds the value of "this" to the original function.
+// - Function.prototype.call and Function.prototype.apply, which call the original function with a specific "this" value and arguments.
+// - setTimeout and setInterval, which take a function as an argument and execute it after a specified delay or interval.
+// - Promise.prototype.then, which takes a function as an argument and returns a new promise that resolves when the original promise resolves.
